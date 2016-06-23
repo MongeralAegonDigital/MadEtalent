d0f9177c29f90adaad64a5bb155c40545a19bec1
Jonathan Iqueda <jiqueda@mongeralaegon.com.br>
2016-06-23 16:33:56 -0300

    Implementando o metodo de correlacao de notas

--- src/Etalent/ManagerEtalentStrategy.php
+++ src/Etalent/ManagerEtalentStrategy.php
@@ -15,8 +15,7 @@ class ManagerEtalentStrategy extends SoapCustomRequest
 
     public function __construct($wsdlUrl, $user, $pass)
     {
-        parent::__construct($wsdlUrl, $user, $pass,
-            [ 'cache_wsdl' => WSDL_CACHE_NONE, 'trace' => true, 'exceptions' => true]);
+        parent::__construct($wsdlUrl, $user, $pass, [ 'cache_wsdl' => WSDL_CACHE_NONE, 'trace' => true, 'exceptions' => true]);
     }
 
     public function getRetornarQuestionario()
@@ -41,8 +40,7 @@ class ManagerEtalentStrategy extends SoapCustomRequest
         try {
             $request = $this->call('GravarCandidatoEtalent', [$user]);
 
-            if (isset($request->GravarCandidatoEtalentResult->RealizadoComSucesso)
-                && $request->GravarCandidatoEtalentResult->RealizadoComSucesso) {
+            if (isset($request->GravarCandidatoEtalentResult->RealizadoComSucesso) && $request->GravarCandidatoEtalentResult->RealizadoComSucesso) {
                 return $request->GravarCandidatoEtalentResult;
             } else {
                 throw new Exception($this->exception);
@@ -81,6 +79,26 @@ class ManagerEtalentStrategy extends SoapCustomRequest
 
             if (isset($request->RetornarMiniRelatorioResult->any)) {
                 $result = (array) simplexml_load_string($request->RetornarMiniRelatorioResult->any);
+                return $result;
+            } else {
+                throw new Exception($this->exception);
+            }
+        } catch (SoapFault $e) {
+            throw new Exception($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function getRetornaCorrelaçãoCandidatos($etalentUserId)
+    {
+        $user         = new stdClass();
+        $user->userId = $etalentUserId;
+
+        // Make the request
+        try {
+            $request = $this->call('RetornaCorrelacaoCandidatos', [$user]);
+
+            if (isset($request->RetornaCorrelacaoCandidatosResult->any)) {
+                $result = (array) simplexml_load_string($request->RetornaCorrelacaoCandidatosResult->any);
                 return $result;
             } else {
                 throw new Exception($this->exception);
